fb99b5202b735f2fa3d11c59772fcc0880f4eaf4
aparc <a.parchukov@yandex.ru>
2018-02-02 00:52:19 +0300

    fix tests

--- src/test/java/org/aparc/productsystem/controller/ProductControllerTest.java
+++ src/test/java/org/aparc/productsystem/controller/ProductControllerTest.java
@@ -1,56 +1,65 @@
-//package org.aparc.productsystem.controller;
-//
-//import org.aparc.productsystem.model.Product;
-//import org.aparc.productsystem.service.ProductService;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.Mockito;
-//import org.mockito.MockitoAnnotations;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.context.web.WebAppConfiguration;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.hamcrest.Matchers.hasSize;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-//
-//public class ProductControllerTest {
-//
-//    @Mock
-//    private ProductService productService;
-//
-//    @InjectMocks
-//    private ProductController productController;
-//
-//    private MockMvc mockMvc;
-//
-//    @BeforeEach
-//    public void setup() {
-//        MockitoAnnotations.initMocks(this);
-//        mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
-//    }
-//
-//    @Test
-//    public void listProductsTest() throws Exception {
-//        List<Product> productList = new ArrayList<Product>();
-//        productList.add(new Product());
-//        productList.add(new Product());
-//
-//        Mockito.when(productService.getAll()).thenReturn(productList);
-//
-//        mockMvc.perform(get("products"))
-//                .andExpect(status().isOk())
-//                .andExpect(view().name("products"))
-//                .andExpect(model().attribute("listProducts",hasSize(2)));
-//    }
-//}
+package org.aparc.productsystem.controller;
+
+import org.aparc.productsystem.model.Product;
+import org.aparc.productsystem.service.ProductService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/spring_config.xml")
+public class ProductControllerTest {
+
+    private ProductService productService = mock(ProductService.class);
+
+    @InjectMocks
+    private ProductController productController;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setSuffix(".jsp");
+
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(productController).dispatchOptions(true).setViewResolvers(viewResolver).build();
+    }
+
+    @Test
+    public void listProductsTest() throws Exception {
+        List<Product> productList = new ArrayList<Product>();
+        Product prod = new Product();
+        productList.add(new Product());
+        productList.add(new Product());
+
+        Mockito.when(productService.getAll()).thenReturn((List)productList);
+
+        mockMvc.perform(get("/products"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("product", notNullValue()))
+                .andExpect(view().name("products"))
+                .andExpect(model().attribute("listProducts",hasSize(2)));
+    }
+}
+
